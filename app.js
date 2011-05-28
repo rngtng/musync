1c49799bd105c7db4d82991ddf884dff217a5ed1
Tobias Bielohlawek <tobi@soundcloud.com>
2011-05-28 20:43:01 +0200

    better counter impl. added player

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', function(req, res){
+u = 'renegadesofjazz%2Fkid-loco-the-passenger'
+app.get('/(:steps)?', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Express',
+    url: 'http%3A%2F%2Fsoundcloud.com%2F'+u+'&amp;enable_api=true&amp;object_id=scPlayer',
+    steps: (req.params.steps ? req.params.steps : 100 )
   });
 });
 
@@ -40,7 +43,6 @@ app.get('/counter/(:steps)?', function(req, res){
     steps: (req.params.steps ? req.params.steps : 100 )
   });
 });
-
 app.listen(3000);
 
 
@@ -51,16 +53,13 @@ serverTime = function() {
 
 var socket = require('socket.io').listen(app);
 socket.on('connection', function(client){
-  client.on('message', function(){
-    client.send(serverTime());
+  client.on('message', function(cmd){
+    if( cmd == 'ping' ){
+      client.send({cmd: cmd, data: serverTime()});
+    }
   });
   //client.on('disconnect', function(){ … })
 });
 
-
-// var everyone = require("now").initialize(app);
-// everyone.now.getTime = function(){
-//   this.now.receiveTime(serverTime());
-// };
 
 console.log("Express server listening on port %d", app.address().port);
